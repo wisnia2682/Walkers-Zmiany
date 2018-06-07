af3cda0ddffd143638102b78e828b66c3e655cd5
wisnia2682 <bw2682@gmail.com>
2018-06-07 13:47:56 +0200

    Update controllerSim.h

--- include/Controlleer/controllerSim.h
+++ include/Controlleer/controllerSim.h
@@ -3,3 +3,5 @@ Plik controller.h nie jest użyty nigdzie? - nie trzeba przenosić
 controllerSim - trzeba przenieść
 
 Odpowiednikiem pliku controller.h jest Board w Walers. nie trzeba przenosic
+
+controllerSim - metody w nim zaimplementowane  trzeba zaimplementowac w simulatorODE.cpp
